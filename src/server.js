2f94456263d15827bf7c2136abc7359641b08fe9
Mike Milner <mike.milner@canonical.com>
2012-08-21 22:28:56 -0300

    Change basic server to use Express.
    Initial change to use Express framework. Added basic middleware setup
    (still needs to be tuned).

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
 var express = require('express');
+var io = require('socket.io');
 var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8000/');
+var app = express();
+
+// Set up middleware
+app.use(express.responseTime());
+app.use(express.logger());
+app.use(express.favicon());
+app.use(express.static(__dirname + '/public'));
+app.use(express.cookieParser());
+app.use(express.session({secret: "BadWolf"}));
+
+// Set up routes
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
+// Start listening
+app.listen(8000);
+console.log('Server listening at http://127.0.0.1:8000/');
